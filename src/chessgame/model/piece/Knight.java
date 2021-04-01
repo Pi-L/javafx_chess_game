88d9ef0539831ea58da5fc0f3a21652f76f0fa4c
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-04-01 21:46:24 +0200

    [WIP] knight moves

--- src/chessgame/model/piece/Knight.java
+++ src/chessgame/model/piece/Knight.java
@@ -17,6 +17,23 @@ public class Knight extends Piece{
     public List<Case> getPossibleCaseList(Case[][] cases, Position position) {
         List<Case> possibleCaseList = new ArrayList<>();
 
+        Case currentCase = cases[position.getX()][position.getY()];
+
+        int[] possibleModifiers = new int[] {-2, -1, 1, 2};
+
+        for (int x : possibleModifiers) {
+            for (int y : possibleModifiers) {
+                if(Math.abs(x) == Math.abs(y)) continue;
+
+                Position movePosition = new Position(position.getX() + x, position.getY() + y);
+                if(movePosition.isOutOfBound()) continue;
+
+                Case moveCase = cases[movePosition.getX()][movePosition.getY()];
+                if(currentCase.isSamePlayer(moveCase)) continue;
+
+                possibleCaseList.add(moveCase);
+            }
+        }
 
         return possibleCaseList;
     }
